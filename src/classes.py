bd5c9c238bb2cf584fe70eb7c9b8b278828d6850
Unknown <piquipato@gmail.com>
2017-10-30 22:32:22 +0100

    Classes Methods
    I've worked on Methods for:
    - Player
    - Property
    - Cash
    - Dice
    I've also created a new object called Bank, which will operate as an artificial intelligence during the game. The bank has a limited amount of Cash, so matches will be much more interesting.

--- src/classes.py
+++ src/classes.py
@@ -22,12 +22,12 @@ Link to Git Repository: https://github.com/Piquipato/Monopoly-in-Python.git
 ------------------------------------------------------------------------------------------------------------------------
 """
 
-# TO DO: Create Classes listed above.
+# TODO: Create Classes listed above.
 
 
 class Player:
 
-    # TO DO: Create Methods to work with Player Data.
+    # TODO: Create Methods to work with Player Data.
 
     def __init__(self, idnum, name, space_in):
 
@@ -42,9 +42,24 @@ class Player:
         self.jail_turns = 0         # Integer. Number of turns to Leave the Jail
 
 
+class Bank(Player):
+
+    # TODO: Create Methods to work with Bank Data.
+
+    def __init__(self):
+
+        # Initializes Bank Object.
+
+        Player.__init__(self, idnum=1, name='Bank', space_in=None)
+        self.cash = Cash(2000000)
+        self.properties = []
+
+
+
+
 class Space:
 
-    # TO DO: Create Methods to work with Space Data.
+    # TODO: Create Methods to work with Space Data.
 
     def __init__(self, position, assigned_property):
 
@@ -65,7 +80,7 @@ class Space:
 
 class Board:
 
-    # TO DO: Create Methods to work with Board Data.
+    # TODO: Create Methods to work with Board Data.
 
     def __init__(self, space, properties):
 
@@ -74,7 +89,7 @@ class Board:
         self.spaces = []
         for i in range(len(properties)):
             for x in properties:
-                if x.id == i:
+                if x.id == space.position:
                     self.spaces.append(space.__init__(i, x))
 
 
@@ -100,10 +115,15 @@ class Dice:
         x = self.value + other.value
         return "{0} and {1} are equal to {2}".format(self.value, other.value, x)
 
+    def __mul__(self, secdice, other):
+
+        x = self.value + secdice.value
+        y = x * other
+        return "{0} and {1} are equal to {2}. {2} times {3} is equal to {4}".format(self.value, secdice.value, x,
+                                                                                    other, y)
+
 
 class Cash:
-
-    # TO DO: Create Mathematical Methods within this class.
 
     """
         This Class Simulates Money in Monopoly.
@@ -115,6 +135,21 @@ class Cash:
 
         self.cash = cash
         self.currency = currency
+
+    def __str__(self):
+        return '{0}{1}'.format(self.cash, self.currency)
+
+    def __neg__(self):
+        return -self.cash
+
+    def __pos__(self):
+        return +self.cash
+
+    def __invert__(self):
+        return 1 / self.cash
+
+    def __abs__(self):
+        return abs(self.cash)
 
     def __add__(self, other):
 
@@ -129,19 +164,164 @@ class Cash:
 
     def __sub__(self, other):
 
-        if (isinstance(other, int)) or (isinstance(other, float)):
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
             return self.cash - other
 
-        elif isinstance(other, Cash):
+        elif isinstance(other, Cash):   # If other is a Cash object.
             return self.cash - other.cash
 
         else:
             raise ValueError('Cash cannot be added up with the requested type: {0}'.format(type(other)))
 
+    def __mul__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            return self.cash * other
+
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            return self.cash * other.cash
+
+        else:
+            raise ValueError('Cash cannot be multiplied with the requested type: {0}'.format(type(other)))
+
+    def __truediv__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            return self.cash / other
+
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            return self.cash / other.cash
+
+        else:
+            raise ValueError('Cash cannot be divided with the requested type: {0}'.format(type(other)))
+
+    def __floordiv__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            return self.cash // other
+
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            return self.cash // other.cash
+
+        else:
+            raise ValueError('Cash cannot be divided with the requested type: {0}'.format(type(other)))
+
+    def __mod__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            return self.cash % other
+
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            return self.cash % other.cash
+
+        else:
+            raise ValueError('Cash cannot be divided with the requested type: {0}'.format(type(other)))
+
+    def __divmod__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            return divmod(self.cash, other)
+
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            return divmod(self.cash, other.cash)
+
+        else:
+            raise ValueError('Cash cannot be divided with the requested type: {0}'.format(type(other)))
+
+    def __eq__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            if self.cash == other:
+                return True
+            else:
+                return False
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            if self.cash == other.cash:
+                return True
+            else:
+                return False
+        else:
+            raise ValueError('Cash cannot be compared with the requested type: {0}'. format(type(other)))
+
+    def __ne__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            if self.cash != other:
+                return True
+            else:
+                return False
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            if self.cash != other.cash:
+                return True
+            else:
+                return False
+        else:
+            raise ValueError('Cash cannot be compared with the requested type: {0}'. format(type(other)))
+
+    def __lt__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            if self.cash < other:
+                return True
+            else:
+                return False
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            if self.cash < other.cash:
+                return True
+            else:
+                return False
+        else:
+            raise ValueError('Cash cannot be compared with the requested type: {0}'. format(type(other)))
+
+    def __le__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            if self.cash <= other:
+                return True
+            else:
+                return False
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            if self.cash <= other.cash:
+                return True
+            else:
+                return False
+        else:
+            raise ValueError('Cash cannot be compared with the requested type: {0}'. format(type(other)))
+
+    def __gt__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            if self.cash > other:
+                return True
+            else:
+                return False
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            if self.cash > other.cash:
+                return True
+            else:
+                return False
+        else:
+            raise ValueError('Cash cannot be compared with the requested type: {0}'. format(type(other)))
+
+    def __ge__(self, other):
+
+        if (isinstance(other, int)) or (isinstance(other, float)):  # If other is an int object or a float object.
+            if self.cash >= other:
+                return True
+            else:
+                return False
+        elif isinstance(other, Cash):   # If other is a Cash object.
+            if self.cash >= other.cash:
+                return True
+            else:
+                return False
+        else:
+            raise ValueError('Cash cannot be compared with the requested type: {0}'. format(type(other)))
+
 
 class Property:
 
-    # TO DO: Create Methods to work with Property data.
+    # TODO: Create Methods to work with Property data.
 
     def __init__(self, name, idnum, price, rent, mortgage, owner=None, mortgage_per=110, ismortgage=False):
 
@@ -156,16 +336,32 @@ class Property:
         self.rebuy_mort = mortgage * (mortgage_per / 100)   # Cash Object. Mortgage Rebuy.
         self.mortgage_status = ismortgage                   # Boolean. Mortgage Status.
 
+    def buy(self, player):
+
+        # This Method will be called when a player buys a property.
+
+        self.owner = player
+        player.properties.append(self)
+        player.cash -= self.price
+
+    def sell(self, newplayer, oldplayer=None):
+
+        if self in oldplayer.properties:
+            oldplayer.properties.remove(self)
+
+        Property.buy(self, newplayer)
+
 
 class Street(Property):     # Inherits from Property.
 
-    # TO DO: Create Methods to work with Street data.
+    # TODO: Create Methods to work with Street data.
 
-    def __init__(self, name, space, color, price, price_building, rent, rent_building, owner=None, mortgage=False):
+    def __init__(self, name, idnum, color, price, price_building, rent, rent_building, mortgage,
+                 owner=None, mortgage_per=110, ismortgage=False):
 
         # Initializes the Street Object
 
-        Property.__init__(name, space, price, rent, owner, mortgage)
+        Property.__init__(name, idnum, price, rent, mortgage, owner, mortgage_per, ismortgage)
 
         self.color = color                      # Color Object. Color of Monopoly.
         self.price_building = price_building    # Tuple with Cash Objects. Building Prices.
@@ -176,11 +372,13 @@ class Street(Property):     # Inherits from Property.
 
 class Railroad(Property):
 
-    # TO DO: Create Methods to work with Railroad Data.
+    # TODO: Create Methods to work with Railroad Data.
 
-    def __init__(self):
+    def __init__(self, name, idnum, price, rent, mortgage, owner=None, mortgage_per=110, ismortgage=False):
 
         # Initializes the Railroad Object
+
+        Property.__init__(self, name, idnum, price, rent, mortgage, owner, mortgage_per, ismortgage)
 
         self.rent_2 = self.rent * 2
         self.rent_3 = self.rent * 3
